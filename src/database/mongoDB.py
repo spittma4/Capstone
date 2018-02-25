50bdddc3bcf26424c9aa93427ca2b82a6424ba82
Jack Dauphars <jdauphar@kent.edu>
2018-02-25 17:10:44 -0500

    create a few functions for interacting with mongoDB

---
+++ src/database/mongoDB.py
@@ -0,0 +1,24 @@
+from pymongo import MongoClient
+from bson.objectid import ObjectId
+
+def set_client(address):
+    return MongoClient(address)
+
+def set_db(client,DBname):
+    return client[DBname]
+
+def set_collection(DB,collection):
+    return DB.[collection]
+
+def read_collection(collection):
+    pre = list(collection.find())
+    for item in pre:
+        del item['_id']
+    result = []
+    for entry in pre:
+        info = (
+            entry['tweet']
+            time['time']
+        )
+        result.append(tuple(info))
+    return result
