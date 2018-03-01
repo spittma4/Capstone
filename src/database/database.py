ef8f6da3195973168ce9c317a76f0498c061a64d
stuart <smckaige91@gmail.com>
2018-03-01 16:11:49 -0500

    unknow error

--- src/database/database.py
+++ src/database/database.py
@@ -13,20 +13,23 @@ class Database:
     def __init__(self):
         self.connection = pymysql.connect(host=DB_PATH, user="root", database="prod")
 
-    def generate_salt():
+    def generate_salt(self):
         salt = ""
         for i in range(0,15):
             salt+=random.choice(string.letters)
         return salt
         
     def sanitize(self, inString):
-        result = 
-    
-    def check_login(self, email, password, db=self.connection):
+        #result =
         pass
 
-    def user_exists(self, email, db=self.connection):
-        cur = db.cursor()
+    def check_login(self, email, password, db=None):
+        pass
+
+    def user_exists(self, email, db=None):
+        if(db==None):
+            db=self.connection
+        cur = db.connection.cursor()
         try:
            result = cur.fetchall("select * from users where email = '{}'").format(email)
            if len(result) > 0:
@@ -36,8 +39,12 @@ class Database:
         except:
             return False, DATABASE_ERROR
 
-    def insert_user(self, email, password, fullname, db=self.connection):
+    def insert_user(self, email, password, fullname, db=None):
+        if(db==None):
+            db=self.connection
         cur = db.cursor()
         try:
             cur.execute("insert into users values()").format(email, password, fullname)
+            return True, None
         except:
+            return False, DATABASE_ERROR
