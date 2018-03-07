7fd301e66885e8e024561a862346ceaafb01cde1
Jack Dauphars <jdauphar@kent.edu>
2018-03-07 17:05:48 -0500

    add check_login and helpers for testing

--- src/database/database.py
+++ src/database/database.py
@@ -8,6 +8,7 @@ import random
 
 DB_PATH = "localhost"
 DATABASE_ERROR = 1
+PASSWORD_MISMATCH = 2
 class Database:
     connection=None
     def __init__(self):
@@ -19,14 +20,46 @@ class Database:
             salt+=random.choice(string.ascii_letters)
         return salt
 
-    """Alledgedy this will just work thru pymysql when we execute a command
-    def sanitize(self, inString):
-        #result =
-        pass
-    """
+    def get_salt(email, db=None):
+        if (db==None):
+            db = self.connection
+        cursor = db.connection.cursor()
+        try:
+            salt = cursor.fetchall("select salt from users where email == {}".format(email))
+            return True, salt
+        except:
+            return False, DATABASE_ERROR
+        
+    
     def check_login(self, email, password, db=None):
-        pass
-
+        if (db==None):
+            db = self.connection
+        if user_exists(email):
+            result, code = verify_password(email, password)
+            if result:
+                return True, None
+            else:
+                return False, code
+            
+    def verify_password(email, password, db=None):
+        if (db==None):
+            db = self.connection
+        result, salt = get_salt()
+        if not result:
+            return False, DATABASE_ERROR
+        password += salt
+        hashed = (hashlib.sha256(password.encode('ascii'))).hexdigest()
+        
+        cursor = db.connection.cursor()
+        try:
+            dbPassword = cursor.fetchall("select password from users where email == {}".format(email))
+            if (dbPassword == hashed):
+                return True, None
+            else:
+                return False, PASSWORD_MISMATCH
+        except:
+            return False, DATABASE_ERROR
+        
     def user_exists(self, email, db=None):
         if(db==None):
             db=self.connection
