9cb14a5fd5f4cd09db767d8013b9ac6dfb489163
Sam Pittman <spittma4@kent.edu>
2018-03-28 19:49:38 -0400

    Update front.py

--- src/front.py
+++ src/front.py
@@ -24,6 +24,25 @@ def get_session():
         return _logins[cookie]
     else:
         redirect('/login')
+        
+# Static Routes --------------------------------------------#
+@get("/static/css/<filepath:re:.*\.css>")
+def css(filepath):
+    return static_file(filepath, root="static/css/")
+
+@get("/static/font/<filepath:re:.*\.(eot|otf|svg|ttf|woff|woff2?)>")
+def font(filepath):
+    return static_file(filepath, root="static/font/")
+
+@get("/static/img/<filepath:re:.*\.(jpg|png|gif|ico|svg)>")
+def img(filepath):
+    return static_file(filepath, root="static/img/")
+
+@get("/static/js/<filepath:re:.*\.js>")
+def js(filepath):
+    return static_file(filepath, root="static/js/")
+
+#------------------------------------------------------------#
 
 @route('/login')
 def login(status=''):
