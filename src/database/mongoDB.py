1a37698a45a23e890ec95ecca190a5d3667aada3
Jack Dauphars <jdauphar@kent.edu>
2018-04-11 23:58:45 +0000

    add functionality to store tweets

--- src/database/mongoDB.py
+++ src/database/mongoDB.py
@@ -1,18 +1,32 @@
 from pymongo import MongoClient
 from bson.objectid import ObjectId
 
-def set_client(address):
-    return MongoClient(address)
+class Mongo:
+    def __init__(self):
+        
 
-def set_db(client,DBname):
-    return client[DBname]
+    def set_client(self, address):
+        self.client = MongoClient(address)
+        
+    def set_db(self, client, DBname):
+        self.db = client[DBname]
+            
+    def set_collection(self, collection):
+        self.collection = self.db.[collection]
 
-def set_collection(DB,collection):
-    return DB.[collection]
+    def read_collection(self):
+        fromMongo = list(self.collection.find())
+        return result
 
-def read_collection(collection):
-    fromMongo = list(collection.find())
-    return result
+    def write_tweets(self, tweets):
+        #store text, retweets and favorites
+        #takes list of tuples
+        for tweet in tweets:
+            self.collection.insert({
+                "_text":tweet[0],
+                "_favCount":tweet[1]
+                "_retweetCount":tweet[2],
+            })
 
-def write_collection(collection,listDict):
-    
+    def write_collection(self, collection, listDict):
+        #store replies' text
